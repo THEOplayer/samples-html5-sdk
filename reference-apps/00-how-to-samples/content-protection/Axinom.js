05621c71ce9549cc2e2b0a475484720682e85ef9
DavidCostaTHEO <56391687+DavidCostaTHEO@users.noreply.github.com>
2020-02-18 15:17:40 +0100

    Create Axinom.js

---
+++ reference-apps/00-how-to-samples/content-protection/Axinom.js
@@ -0,0 +1,35 @@
+if (HLS) {
+    let drmConfiguration = {
+        "integration": "axinom",
+        "token": "<AXINOM_TOKEN>",
+        "fairplay": {
+            "licenseAcquisitionURL": "<LICENSE_KEY_URL_FAIRPLAY>",
+            "certificateURL": "CERTIFICATE_URL>"
+        }
+    };
+    player.source = {
+        "sources": {
+            "src": "<HLS_STREAM_URL>",
+            "type": "application/x-mpegurl",
+            "contentProtection": drmConfiguration
+        }
+    }
+} else if (DASH) {
+    let drmConfiguration = {
+        "integration": "axinom",
+        "token": "<AXINOM_TOKEN>",
+        "playready": {
+            "licenseAcquisitionURL": "<LICENSE_KEY_URL_PLAYREADY>"
+        },
+        "widevine": {
+            "licenseAcquisitionURL": "<LICENSE_KEY_URL_WIDEVINE>"
+        }
+    };
+    player.source = {
+        "sources": {
+            "src": "<DASH_STREAM_URL>",
+            "type": "application/dash+xml",
+            "contentProtection": drmConfiguration
+        }
+    }
+}
