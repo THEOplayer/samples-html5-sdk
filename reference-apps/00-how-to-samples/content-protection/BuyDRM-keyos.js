0811c866a4b2a9f8d7c4de1f170098e9258dffb1
DavidCostaTHEO <56391687+DavidCostaTHEO@users.noreply.github.com>
2020-02-18 13:51:57 +0100

    Create BuyDRM-keyos.js

---
+++ reference-apps/00-how-to-samples/content-protection/BuyDRM-keyos.js
@@ -0,0 +1,35 @@
+if (HLS) {
+    let drmConfiguration = {
+        "integration": "keyos",
+        "customdata": "<CUSTOMDATA>",
+        "fairplay": {
+            "licenseAcquisitionURL": "<LICENSE_KEY_URL_FAIRPLAY>",
+            "certificateURL": "CERTIFICATE_URL>"
+        }
+    };
+    player.source = {
+        "sources": {
+            "src": "<HLS_STREAM_URL>",
+            "type": "application/x-mpegurl",
+            "contentProtection": drmConfiguration
+        }
+    }
+} else if (DASH) {
+    let drmConfiguration = {
+        "integration": "keyos",
+        "customdata": "<CUSTOMDATA>",
+        "playready": {
+            "licenseAcquisitionURL": "<LICENSE_KEY_URL_PLAYREADY>"
+        },
+        "widevine": {
+            "licenseAcquisitionURL": "<LICENSE_KEY_URL_WIDEVINE>"
+        }
+    };
+    player.source = {
+        "sources": {
+            "src": "<DASH_STREAM_URL>",
+            "type": "application/dash+xml",
+            "contentProtection": drmConfiguration
+        }
+    }
+}
